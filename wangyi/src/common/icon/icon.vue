927cec7243e3bc6450954b7a0ac5dd5cb17653f5
FENGXIUWEI <1428915751@qq.com>
2019-01-18 16:32:29 +0800

    歌曲

---
+++ wangyi/src/common/icon/icon.vue
@@ -0,0 +1,35 @@
+<template>
+   <i :class="`iconfont icon-${type}`" :style="bgStyle"></i>
+</template>
+
+<script>
+export default {
+  name: 'icon',
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: Number,
+      default: 16
+    },
+    color: {
+      type: String,
+      default: '#5c6b77'
+    }
+  },
+  computed: {
+    bgStyle () {
+      return {
+        fontSize: `${this.size}px`,
+        color: this.color
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- wangyi/src/components/wyheader/wyheader.vue
+++ wangyi/src/components/wyheader/wyheader.vue
@@ -2,26 +2,49 @@
   <div class="header-wrap" ref="headerWrap">
     <div class="icon">
       <a href="#">
-        <i class="iconfont icon-yuyin"></i>
+        <Icon :type="yuyin.type" :size="yuyin.size" :color="yuyin.color"></Icon>
       </a>
     </div>
     <div class="input">
       <span>
-        <i class="iconfont icon-chaxun"></i>
+        <Icon :type="chaxun.type" :size="chaxun.size" :color="chaxun.color"></Icon>
         搜索音乐,歌词
       </span>
     </div>
     <div class="icon">
       <a href="#">
-        <i class="iconfont icon-zhengzaibofang"></i>
+        <Icon :type="play.type" :size="play.size" :color="play.color"></Icon>
       </a>
     </div>
   </div>
 </template>
 
 <script>
+import Icon from '@/common/icon/icon'
 export default {
-  name: 'WyHeader'
+  name: 'WyHeader',
+  data () {
+    return {
+      yuyin: {
+        type: 'yuyin',
+        size: 24,
+        color: 'white'
+      },
+      chaxun: {
+        type: 'chaxun',
+        size: 16,
+        color: '#88898d'
+      },
+      play: {
+        type: 'zhengzaibofang',
+        size: 24,
+        color: 'white'
+      }
+    }
+  },
+  components: {
+    Icon
+  }
 }
 </script>
 
